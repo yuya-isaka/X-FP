aefd70b4c3b96d804ac1d06d559b2865c73edcdf
yuya-isaka <zxwg6329@gmail.com>
2021-12-02 07:52:22 +0900

    add

--- Benchmark/project_benchmark/project_benchmark.sim/sim_1/synth/func/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-call %xv_path%/xelab  -wto 575487e5f0174d70ba47e8736bf9073b -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L secureip --snapshot lfsr_func_synth xil_defaultlib.lfsr xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- Benchmark/project_benchmark/project_benchmark.sim/sim_1/synth/func/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-call %xv_path%/xsim lfsr_func_synth -key {Post-Synthesis:sim_1:Functional:lfsr} -tclbatch lfsr.tcl -view H:/Benchmark/project_benchmark/lfsr_func_synth.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
